2858ef6c82c971ec99c67d73ad82c138cb3975f8
ihimran4 <ihimran408@gmail.com>
2025-09-18 12:04:24 +0600

    added call history record and coin count

--- home.js
+++ home.js
@@ -7,3 +7,55 @@ for (const heart of hearts) (
         document.getElementById('heart-nav').innerText = currentHearts;
     })
 )
+
+function getElements(id) {
+    const element = document.getElementById(id)
+    return element;
+}
+// call alert , call history ,coin count
+const callBtn = document.getElementsByClassName('call-btn')
+
+for (let callButton of callBtn) {
+    callButton.addEventListener("click", function () {
+        // coin count
+        const coin = getElements('coin').innerText;
+
+        const currentCoin = Number(coin) - 20;
+        if (currentCoin < 20) {
+            alert('coin used')
+            return;
+        }
+        getElements('coin').innerText = currentCoin;
+
+
+        // by childnodes
+        const cardTitle = callButton.parentNode.parentNode.childNodes[3].innerText;
+
+        // by children
+        const cardCall = callButton.parentNode.parentNode.children[3].innerText;
+
+        let time = new Date()
+        let currentTime = time.toLocaleTimeString()
+        // console.log(currentTime)
+
+        const callHistory = getElements('call-history')
+
+        const newHistory = document.createElement("div")
+        newHistory.innerHTML = ` 
+         <div class="flex justify-between items-center gap-3 p-4 bg-gray-100
+         mb-4 rounded-lg ">
+                    <div class = "">
+                        <h1 class="text-2xl">${cardTitle}</h1>
+                        <h2 class="text-xl text-gray-400">${cardCall}</h2>
+                    </div>
+                    <P class="text-lg  ">${currentTime}</P>
+                </div>
+                
+                `;
+
+        callHistory.append(newHistory);
+
+        alert('calling'+" "+cardTitle+" "+cardCall);
+
+    })
+}
